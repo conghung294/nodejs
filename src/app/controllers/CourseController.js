e49ae8bde6375baef706ca4ce7d6df062216351f
conghung294 <conghung294@gmail.com>
2023-10-15 00:50:24 +0700

    create course

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -6,11 +6,21 @@ class CourseController {
     show(req, res,next) {
         Course.findOne({ slug: req.params.slug })
           .then(course => {
-            res.render('\courses/show',{
+            res.render('courses/show',{
                 course:mongooseToObject(course)
             })
           })
           .catch(next)
+    }
+    create(req,res,next){
+      res.render('courses/create')
+    }
+    store(req,res,next){
+     
+      const course = new Course(req.body)
+      course.save()
+        .then(()=>res.redirect('/'))
+        .catch(next)
     }
 }
 
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,14 +1,18 @@
 const mongoose = require('mongoose');
-
-
+const slug = require('mongoose-slug-generator')
+mongoose.plugin(slug)
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: {type:String,maxLength:255},
+    name: {type:String,required:true},
     description: {type:String,maxLength:255},
     image: {type:String,maxLength:255},
-    creatAt: {type:Date,default:Date.now},
-    updateAt: {type:Date,default:Date.now}
-})
+    videoiD:{type:String,maxLength:255},
+    slug: { type: String, slug: "name" , unique:true}
+},
+{
+    timestamps:true
+}
+)
 
 module.exports= mongoose.model('Course',Course)
